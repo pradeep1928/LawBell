e8abc3dbebbfb5844dd65aeb2ba46abf3ea017d1
Pradeep Padmukhi <ppadmukhi28@gmail.com>
2022-05-14 13:52:58 +0530

    express server setup and mongodb localhost connection

---
+++ backend/index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const http = require('http');
+const dotenv = require("dotenv");
+
+const connectDB = require('./config/db');
+
+// Configurations
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+connectDB()                       // connection to mongodb
+
+
+// Routes and endpoints 
+app.get("/", (req, res) => {
+    console.log("Welcome to LawBell");
+    res.send("<h1>Welcome to LawBell</h1>");
+})
+
+
+// creating server instance using http 
+const server = http.createServer(app);
+
+
+// listening the server at port 5000
+const port = process.env.PORT || 5000;
+server.listen(port, () => {
+    console.log(`server is running at port ${port}`);
+})
