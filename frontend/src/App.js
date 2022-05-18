7808484722b6b1cefc6bbcc50d2b10c989bf9d69
Santosh Kumar <pleaseforwardyourmail.com>
2022-05-19 00:22:31 +0530

    Upgraded React Router Dom from V5 to V6 use ' npm install react-router-dom@6'

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
-// import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./Components/Navbar";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from './Components/pages';
 import About from './Components/pages/about';
 import Contact from './Components/pages/contact';
@@ -10,16 +9,16 @@ import SignIn from './Components/pages/signin';
 
 function App() {
   return (
-    <Router>
+    <BrowserRouter>
       <Navbar />
-      <Switch>
+      <Routes>
         <Route path="/" exact component={Home} />
         <Route path="/about" component={About} />
         <Route path="/contact" component={Contact} />
         <Route path="/signin" component={SignIn} />
         <Route path="/sign-up" component={SignUp} />
-      </Switch>
-    </Router>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
