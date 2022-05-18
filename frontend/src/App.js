75815f8ad3a90b60fe2cb5530eafff9d329bd1d1
Santosh Kumar <pleaseforwardyourmail.com>
2022-05-19 00:10:19 +0530

    Removeds App.css and import of App.Css in App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import './App.css';
+// import './App.css';
 import Navbar from "./Components/Navbar";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from './Components/pages';
